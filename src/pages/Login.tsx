ae6374de2dbc06dd722eb21c3a83b8534c9d36ad
ND-Kubayi <kubayintsumid@gmail.com>
2025-08-31 12:46:22 +0200

    added social media links

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -47,9 +47,6 @@ export const Login = () => {
               className="bg-gray-200 rounded-md p-2 w-full mb-4 outline-none text-md"
             />
 
-            <div className="flex space-x-4 mb-4 text-gray-600 text-lg">
-              
-            </div>
 
             <button
               type="submit"
@@ -68,7 +65,7 @@ export const Login = () => {
               : "translate-x-0 opacity-100 z-10"
           }`}
         >
-          <form className="flex flex-col items-center justify-center w-full">
+          <form className="flex flex-col items-center justify-center w-full pb-6">
             <h1 className="text-4xl text-[#012250] font-bold mb-4">Welcome Back</h1>
             <input
               type="email"
@@ -126,6 +123,23 @@ export const Login = () => {
               </>
             )}
           </div>
+          {/* social media platforms */}
+          <div className="absolute bottom-2 flex flex-col items-center w-full">
+            <div className="flex space-x-4 mb-4 text-gray-600 text-lg">
+              <h2 className="text-white font-semibold">Socials : </h2>
+              <a href="https://github.com/Kubayi-ND" target="_blank" >
+                <div  className="bg-black rounded-full p-1.5 text-[#bcbcbcff]" id='github'>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#bcbcbcff" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-github-icon lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"/><path d="M9 18c-4.51 2-5-2-7-2"/></svg>
+                </div>
+              </a>
+              <a href="https://www.linkedin.com/in/ntsumi-kubayi/" target="_blank" >
+                <div className="bg-blue-600 rounded-md p-1 text-white">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="white" stroke="currentColor" stroke-width="0" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-linkedin-icon lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg>
+                </div>
+              </a>   
+            </div>
+          </div>
+          
         </div>
       </div>
     </div>
